676e280c63c7a5cd8ae47f2c3d198f4958dc7af7
IgorZasypkin <zas.i.e@yandex.ru>
2020-11-19 11:26:13 +0300

    Main.java

--- src/ATM.java
+++ src/ATM.java
@@ -1,3 +1,18 @@
 public class ATM {
 
+    private int amountMoney;
+
+    public ATM(int amountMoney) {
+        this.amountMoney = amountMoney;
+    }
+
+    int depositMoney(int takeSum) {
+        amountMoney= amountMoney + takeSum;
+        return amountMoney;
+    }
+    int withdrawCash (int takeOffSum) {
+        amountMoney = amountMoney - takeOffSum;
+        return amountMoney;
+    }
+
 }
